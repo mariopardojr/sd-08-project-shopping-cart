1d8711727da8a4dba17b73415282dcdef2af60a2
Daniel <danieldsrd@gmail.com>
2020-12-10 18:02:54 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -27,20 +27,21 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener() {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  document.querySelector('.cart__items').appendChild(li);
+  return li;
+}
 
-async function setFetch() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+function setFetch() {
+  return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then(r => r.json())
     .then((data) => {
       data.results.forEach((elem) => {
@@ -54,6 +55,29 @@ async function setFetch() {
     });
 }
 
-window.onload = function onload() {
-  setFetch();
+function fetchId(id) {
+  return fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then(r => r.json())
+  .then((data) => {
+    const obj = {
+      sku: data.id,
+      name: data.title,
+      salePrice: data.price,
+    };
+    createCartItemElement(obj);
+  });
+}
+
+function buttonsAdd() {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      fetchId(document.querySelectorAll('.item__sku')[index].innerText);
+    });
+  });
+}
+
+window.onload = async function onload() {
+  await setFetch();
+  await buttonsAdd();
 };
