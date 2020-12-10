b3565780fd68695b2c1d855a63c810d51b1b4e1e
Bruno Martins <bruno.martins217@gmail.com>
2020-12-10 19:20:28 -0300

    Requisito 1, first try

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() {};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,6 +24,24 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+const productList = () => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador').then(
+    (response) => {
+      response.json().then((data) => {
+        const arrayData = data.results;
+        arrayData.map((produtos) => {
+          const upItens = createProductItemElement({
+            sku: produtos.id,
+            name: produtos.tittle,
+            image: produtos.thumbnail,
+          });
+          return document.querySelector('.items').appendChild(upItens);
+        });
+      });
+    }
+  );
+};
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -41,3 +57,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  productList();
+};
