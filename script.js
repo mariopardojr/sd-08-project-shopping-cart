9a454bb33952965c09b9888acc8e217c82ad5308
Leandro Lachini <leandrolachini@gmail.com>
2020-12-10 17:55:38 -0300

    iniciando projeto #vqv

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  
   return e;
 }
 
