3d7ed243869a3f797394e22ac6a5b5bdeede6ca6
Vitor Nunes <vitornunes200@gmail.com>
2020-12-10 19:17:25 -0300

    Requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -41,3 +39,49 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const getItemsFromApi = async () => {
+  const sectionItens = document.querySelector('.items');
+  return new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+      .then((response) => {
+        response.json().then((data) => {
+          const resultado = data.results;
+          resultado.forEach((element) => {
+            sectionItens.appendChild(createProductItemElement({
+              sku: element.id,
+              name: element.title,
+              image: element.thumbnail,
+            }));
+          });
+          resolve();
+        });
+      });
+  })
+}
+
+const getSingleItem = (itemId) => {
+  const listItens = document.querySelector('.cart__items');
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    .then((response) => {
+      response.json().then((data) => {
+        listItens.appendChild(createCartItemElement({
+          sku: data.id,
+          name: data.title,
+          salePrice: data.price,
+        }))
+      });
+    });
+}
+
+window.onload = async function onload() {
+  await getItemsFromApi();
+  const allButtons = document.querySelectorAll('.item__add');
+  allButtons.forEach((element) => {
+    element.addEventListener('click', () => {
+      const elementId = element.parentNode.firstChild.innerText;
+      getSingleItem(elementId);
+    })
+  })
+};
+
