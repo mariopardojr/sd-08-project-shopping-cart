811416db92beb8baa0903785ded0219f057a9437
Paulo Simões <paulohbsimoes@gmail.com>
2020-12-10 15:33:37 -0300

    Requisito 1 concluido

--- script.js
+++ script.js
@@ -26,18 +26,32 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const createItems = async () => {
+  const items = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(response => response.json())
+    .then(data => data.results);
+
+  const itemsContainer = document.querySelector('.items');
+  items.forEach((item) => {
+    const { id: sku, title: name, thumbnail: image } = item;
+    itemsContainer.appendChild(createProductItemElement({ sku, name, image }));
+  });
+};
+
+createItems();
