38bd48b95643d2a99c23742345796e9084e57c62
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-10 18:32:09 -0300

    REQ-1: Finished

--- script.js
+++ script.js
@@ -26,18 +26,37 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+// Requirements below:
+const createProductsList = (product) => {
+  const itemsSection = document.querySelector('.items');
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+    .then(response => response.json())
+    .then(data =>
+      data.results.forEach(({ id, title, thumbnail }) => {
+        const obj = {
+          sku: id,
+          name: title,
+          image: thumbnail,
+        };
+        itemsSection.appendChild(createProductItemElement(obj));
+      }),
+    );
+};
+
+window.onload = async () => createProductsList('computador');
