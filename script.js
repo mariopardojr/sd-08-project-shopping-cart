e77636bd8d5c41eedae2560cc0fbaa2d5a963c8f
fabio-infotech <fabio35547@gmail.com>
2020-12-10 19:20:16 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const urlMLGetListOfProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+const urlMLGetProductById = 'https://api.mercadolibre.com/items/';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,6 +15,27 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Requisito 2
+function mlGetProductByID(productId) {
+  fetch(`${urlMLGetProductById}${productId}`)
+    .then(response => response.json())
+    .then((data) => {
+      document.querySelector('.cart__items').appendChild(createCartItemElement(data));
+    });
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,25 +43,17 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const btnAddCartProduct = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  btnAddCartProduct.addEventListener('click', (event) => {
+    mlGetProductByID(getSkuFromProductItem(event.target.parentNode));
+  });
+  section.appendChild(btnAddCartProduct);
+ 
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 // Requisito 1
