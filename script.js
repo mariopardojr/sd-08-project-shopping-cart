9dfadb423b77027cee731a0c665d522301f2e3e9
MEianelli <filipelouzano@gmail.com>
2020-12-10 16:55:08 -0300

    Todos requisitos, time to fight codeclimate

--- script.js
+++ script.js
@@ -1,19 +1,75 @@
 window.onload = function onload() {
   fetchDataFromMLB();
+  loadLocalStorageCart();
+  applyEventTocartItems();
+  addEventToLimparCarrinho();
 };
+
+async function updatePrice() {
+  const currentCartitems = document.querySelectorAll('.cart__item');
+  let totalPrice = 0;
+  currentCartitems.forEach(element => {
+    const text = element.innerHTML;
+    const price = +text.slice(text.lastIndexOf('$') + 1);
+    totalPrice += price;
+  });
+  const totalPriceP = document.querySelector('.total-price');
+  totalPrice = Math.floor(totalPrice * 100) / 100;
+  totalPriceP.innerHTML = `R$${totalPrice}`;
+}
+
+function saveCartToLocal() {
+  const olContainer = document.querySelector('.cart__items');
+  localStorage.setItem('cart', olContainer.innerHTML);
+}
+
+function updateLocalStorage() {
+  updatePrice();
+  localStorage.clear();
+  saveCartToLocal();
+}
+
+function limparCarrinho() {
+  const olContainer = document.querySelector('.cart__items');
+  olContainer.innerHTML = '';
+  updateLocalStorage();
+}
+
+function addEventToLimparCarrinho() {
+  const btnLimpar = document.querySelector('.empty-cart');
+  btnLimpar.addEventListener('click', limparCarrinho);
+}
+
+function loadLocalStorageCart() {
+  const olContainer = document.querySelector('.cart__items');
+  const cartLocalData = localStorage.getItem('cart');
+  olContainer.innerHTML = cartLocalData;
+  updatePrice();
+}
+
+function applyEventTocartItems() {
+  const currentCartitems = document.querySelectorAll('.cart__item');
+  currentCartitems.forEach(item =>
+    item.addEventListener('click', event => cartItemClickListener(event))
+  );
+}
 
 async function fetchDataFromMLB() {
   const containerItems = document.querySelector('.items');
-  const data = fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador').then(result =>
-    result.json().then(data =>
-      data.results.forEach(product => {
-        const productHTML = createProductItemElement(product);
-        const buttonAdicionar = productHTML.childNodes[3];
-        buttonAdicionar.addEventListener('click', e => addToCart(e.path[1]));
-        containerItems.appendChild(productHTML);
-      })
-    )
+  const loadingDiv = document.querySelector('.loading ');
+  loadingDiv.style.display = 'block';
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador').then(
+    result =>
+      result.json().then(data =>
+        data.results.forEach(product => {
+          const productHTML = createProductItemElement(product);
+          const buttonAdicionar = productHTML.childNodes[3];
+          buttonAdicionar.addEventListener('click', e => addToCart(e.path[1]));
+          containerItems.appendChild(productHTML);
+        })
+      )
   );
+  loadingDiv.style.display = 'none';
 }
 
 function createProductImageElement(imageSource) {
@@ -53,22 +109,32 @@ async function fetchItemData(itemId) {
 async function addToCart(event) {
   const olContainer = document.querySelector('.cart__items');
   const itemId = getSkuFromProductItem(event);
+  const loadingDiv = document.querySelector('.loading ');
+  loadingDiv.style.display = 'block';
   const itemData = await fetchItemData(itemId).then(itemdata =>
     itemdata.json().then(result => {
       const itemCart = createCartItemElement(result);
       olContainer.appendChild(itemCart);
     })
   );
+  loadingDiv.style.display = 'none';
+  updatePrice();
+  saveCartToLocal();
 }
 
-async function cartItemClickListener(event) {
+function cartItemClickListener(event) {
   const olContainer = event.path[1];
   const cartItem = event.path[0];
   olContainer.removeChild(cartItem);
+  updatePrice();
+  updateLocalStorage();
 }
 
 function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
+  /*   const pId = document.createElement('p');
+  const pTitle = document.createElement('p');
+  const pPrice = document.createElement('p'); */
   li.className = 'cart__item';
   li.innerText = `Id: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', e => cartItemClickListener(e));
