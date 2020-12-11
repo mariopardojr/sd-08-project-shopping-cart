16216a0ca96789739a3ca3681b5a8691ddbe09df
Tandy <tandyfw@gmail.com>
2020-12-11 09:23:16 -0300

    Requisito 1 concluido... eu acho

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  api();
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -24,15 +26,15 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -40,4 +42,27 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+};
+
+const api = () => {
+  return new Promise((resolve, reject) => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then(response => response.json())
+  .then(data => data.results)
+  .then(array => array.map(product => ({
+    sku: product.id,
+    name: product.title,
+    image: product.thumbnail,
+  })))
+  .then(newArray => addProductsOnScreen(newArray))
+  .then(results => resolve(results))
+  .catch(results => reject(console.log('Não faço ideia!')));
+  });
+};
+
+const addProductsOnScreen = (array) => {
+  const items = document.querySelector('.items')
+  array.forEach(element => {
+    items.appendChild(createProductItemElement(element));
+  });
 }
