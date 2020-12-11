391728dfda20c3acb0b2afde553a13f19b8b07e1
Igor Nascimento <igorln96@gmail.com>
2020-12-11 16:56:13 -0300

    req 2 - Adicione o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -14,22 +14,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -40,6 +24,35 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const addToCartButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  addToCartButton.addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${event.target.parentNode.firstChild.innerText}`)
+    .then(result => result.json())
+    .then((data) => {
+      const product = {
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      };
+      const cartItem = createCartItemElement(product);
+      document.getElementsByClassName('cart__items')[0].appendChild(cartItem);
+    });
+  });
+  section.appendChild(addToCartButton);
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function createProductItemList(QUERY) {
