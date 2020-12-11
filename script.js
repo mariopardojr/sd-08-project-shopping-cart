a7a674100ae785ad92c9f40d6f48662b49cb3832
Leandro Lachini <leandrolachini@gmail.com>
2020-12-11 15:29:42 -0300

    requisito 1 => isOk

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 window.onload = function onload() { };
 
+// const getSection = () => {
+//   const sectionProducts = 'computador';
+//   return getProducts(sectionProducts)
+// }
+
+// getSection();
+
+const getProducts = (product) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.results);
+    data.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+      // console.log(sku, name, image);
+      const sectionItems = document.querySelector('.items');
+      // console.log(sectionItems);
+      sectionItems.appendChild(createProductItemElement({ sku, name, image }));
+    });
+  });
+}
+getProducts('computador');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -27,18 +49,18 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
