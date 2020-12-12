afdfd2d7535975303ef75462c972f92d66d2e7c9
Tiago <tiagaoalb@gmail.com>
2020-12-12 02:57:46 -0300

    added 3rd requirement

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function cartItemClickListener() {
-  // coloque seu código aqui
+function cartItemClickListener(event) {
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
