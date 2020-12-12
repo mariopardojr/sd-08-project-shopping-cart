ac4b1bf273cc2df45794b17e08460c1029b1e069
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-12-12 17:10:31 -0300

    R02 - Concluido

--- script.js
+++ script.js
@@ -12,24 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-/* function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  return event;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -38,7 +22,26 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}*/
+}
+
+function createProductItemElement({ sku, name, image, salePrice }) {
+  const cartItems = document.querySelector('.cart__items');
+  const section = document.createElement('section');
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(button);
+  button.addEventListener('click', () => cartItems.appendChild(createCartItemElement({ sku, name, salePrice })));
+
+  return section;
+}
+
+/* function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+} */
 
 window.onload = function onload() {
   const items = document.querySelector('.items');
@@ -50,6 +53,7 @@ window.onload = function onload() {
             sku: product.id,
             name: product.title,
             image: product.thumbnail,
+            salePrice: product.price,
           };
           items.appendChild(createProductItemElement(productData));
         });
