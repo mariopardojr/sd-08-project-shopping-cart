39e7641f69cce25b64885951776a98ba5ed1a38d
rafael <rafaelcostamoura@gmail.com>
2020-12-13 10:23:39 -0300

    Rafael Moura Iniciando Projeto

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
-const query = 'computador';
-const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-const buscaItem = id => fetch(`https://api.mercadolibre.com/items/${id}`)
-  .then(response => response.json())
-  .then(data => data);
+window.onload = function onload() { };
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -19,16 +14,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-async function cartItemClickListener(e) {
-  const containerChart = document.querySelector('.cart__items');
-  const item = e.target;
-  containerChart.removeChild(item);
-  const itemId = item.id;
-  localStorage.removeItem(itemId);
+function cartItemClickListener(event) {
+  // coloque seu código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -38,60 +41,3 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
-async function addItemChart(e) {
-  const idItem = e.target.parentNode.firstChild.innerText;
-  const containerChart = document.querySelector('.cart__items');
-  const consultaItem = await buscaItem(idItem);
-  const { id: sku, title: name, price: salePrice } = consultaItem;
-  const chartItem = createCartItemElement({ sku, name, salePrice });
-  chartItem.id = sku;
-  containerChart.appendChild(chartItem);
-  localStorage.setItem(sku, name);
-}
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
-    .addEventListener('click', addItemChart);
-  return section;
-}
-const newFetch = async () => {
-  const items = await fetch(endPoint)
-    .then(element => element.json())
-    .then(result => result.results);
-  const containeritem = document.querySelector('.items');
-  items.forEach((item) => {
-    const { id: sku, title: name, thumbnail: image } = item;
-    const productItem = createProductItemElement({ sku, name, image });
-    containeritem.appendChild(productItem);
-  });
-};
-newFetch();
-
-window.onload = function onload() {
-  async function addItemChartLocalStorade(idItem) {
-    const containerChart = document.querySelector('.cart__items');
-    const consultaItem = await buscaItem(idItem);
-    const { id: sku, title: name, price: salePrice } = consultaItem;
-    const chartItem = createCartItemElement({ sku, name, salePrice });
-    chartItem.id = sku;
-    containerChart.appendChild(chartItem);
-    localStorage.setItem(sku, name);
-  }
-
-  if (localStorage !== null) {
-    const itemsLocalStorage = Object.keys(localStorage);
-    itemsLocalStorage.forEach(id => addItemChartLocalStorade(id));
-  }
-
-  document.querySelector('.empty-cart').addEventListener('click', (e) => {
-    localStorage.clear();
-    document.querySelector('.cart__items').innerHTML = '';
-  });
-};
