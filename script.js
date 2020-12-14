434cb12ec932e235a3d5c6c8a6962932e02cc8e9
Leandro Lachini <leandrolachini@gmail.com>
2020-12-14 20:06:55 -0300

    requisito 5 code1

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ const cartListSaved = () => {
 const priceTotal = () => {
   const cartList = document.querySelectorAll('.cart__item');
   let price = 0;
-  cartList.forEach(element => {
+  cartList.forEach(element => price += parseFloat(element.innerHTML.split('$')[1]));
     // console.log(price);
-    return price += parseFloat(element.innerHTML.split('$')[1]);
-  });
-  document.querySelector('.total-price').innerText = `Preço Total: $${price}`;
+  document.querySelector('.total-price').innerText = `Preço Total: $${(price).toLocaleString({maximumFractionDigits:2})}`;
   // console.log(cartList);
 };
 
