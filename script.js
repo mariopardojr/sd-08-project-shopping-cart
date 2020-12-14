3f7b5561ff4fcb55c3b3626b61f09ff45aaced4d
Ruan Miguel <ruanmiguelps@gmail.com>
2020-12-14 09:53:26 -0300

    Go

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {}
+function cartItemClickListener(event) 
 
 function cartItemClickListener() {
   document.querySelectorAll('.cart__item').forEach(element => element.addEventListener('click', (event) => {
