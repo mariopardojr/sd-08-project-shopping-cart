679a0cae9731846ed139903516f1d3e67415caa7
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-12-14 08:52:22 -0300

    Req2: Finalizado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.onload = function onload() { };
+// window.onload = function onload() { };
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -15,13 +15,30 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section
+    .appendChild(
+      createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+    ) // req 2 - Criando um evento quando o botão é clicado irei enviar informações como id, nome e preço para a o carrinho de compras
+    .addEventListener("click", () => {
+      fetch(`https://api.mercadolibre.com/items/${sku}`)
+        .then((resolve) => resolve.json())
+        .then((data) => {
+          const cartItem = {
+            sku,
+            name,
+            salePrice: data.price,
+          };
+          console.log("pko");
+          const car = document.querySelector(".cart__items")
+          car.appendChild(createCartItemElement(cartItem));
+        });
+    });
 
   return section;
 }
@@ -30,32 +47,33 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement("li");
+  li.className = "cart__item";
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener("click", cartItemClickListener);
+  return li;
+}
 
 // Req1 - Criando os itens a partir da API e ligando com a função createProductItemElement
 const products = (search) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
-  .then(response => response.json())
-  .then(data => data.results.forEach(({ id, title, thumbnail }) => {
-    const object = {
-      sku: id,
-      name: title,
-      image: thumbnail,
-    };
-    const items = document.querySelector('.items');
-    items.appendChild(createProductItemElement(object));
-  }));
+    .then((response) => response.json())
+    .then((data) =>
+      data.results.forEach(({ id, title, thumbnail }) => {
+        const object = {
+          sku: id,
+          name: title,
+          image: thumbnail,
+        };
+        const items = document.querySelector(".items");
+        items.appendChild(createProductItemElement(object));
+      })
+    );
 };
 
-
-window.onload = async () => products('computador');
+window.onload = async () => products("computador");
