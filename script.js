89cb802e2c996b9755e14a0bdad9a667ba67e523
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-12-14 15:17:17 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-window.onload = function onload() {
-  products();
-};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -17,7 +14,6 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-
   const section = document.createElement('section');
   section.className = 'item';
   section.appendChild(createCustomElement('span', 'item__sku', sku));
@@ -27,33 +23,38 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 const products = () => {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-  .then(response => response.json())
-  .then((data) => { data.results.forEach((product) => {
-    const object = {
-    sku: product.id,
-    name: product.title,
-    image: product.thumbnail,
-    };
+    .then(response => response.json())
+    .then((data) => { 
+      data.results.forEach(product => {
+      const object = {
+        sku: product.id,
+        name: product.title,
+        image: product.thumbnail,
+      };
     const items = document.querySelector('.items');
     items.appendChild(createProductItemElement(object));
-  }); 
+  });
 });
+};
+
+window.onload = function onload() {
+  products();
 };
