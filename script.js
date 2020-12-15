5b11d59ed6618daf6920b2cd9f73e26ecf97be6b
Joao <joaocarlitoneto@gmail.com>
2020-12-15 19:14:11 -0300

    Req 03

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
