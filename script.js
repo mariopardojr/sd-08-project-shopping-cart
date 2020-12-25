72da16304f577f5993b54df3d973047849d899e8
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2020-12-25 20:05:28 -0300

    Task 01 v02 + Task 02 and 03 v01

--- script.js
+++ script.js
@@ -24,36 +24,64 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-async function fetchMlbItemsById(idsArr) {
-  const itemsData = idsArr.map(async id =>
-  fetch(`https://api.mercadolibre.com/items/${id}`)
-    .then(resp => resp.json())
-    .then((itemObj) => {
-      const itemData = {
-        sku: itemObj.id,
-        name: itemObj.title,
-        image: itemObj.pictures[0].url,
-        price: itemObj.price,
-      };
-      return itemData;
-    })
-    .catch(() => { throw new Error('Erro ao consutar o item por id'); }));
-  return Promise.all(itemsData);
-}
+// async function fetchMlbItemsById(idsArr) {
+//   const itemsData = idsArr.map(async id =>
+//   fetch(`https://api.mercadolibre.com/items/${id}`)
+//     .then(resp => resp.json())
+//     .then((itemObj) => {
+//       const itemData = {
+//         sku: itemObj.id,
+//         name: itemObj.title,
+//         image: itemObj.pictures[0].url,
+//         price: itemObj.price,
+//       };
+//       return itemData;
+//     })
+//     .catch(() => { throw new Error('Erro ao consutar o item por id'); }));
+//   return Promise.all(itemsData);
+// }
 
 async function fetchMlbSearch(query) {
-  const itemsId = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
     .then(resp => resp.json())
-    .then(resp => resp.results.map(({ id }) => id))
+    .then(resp => resp.results.map(({ id, title, thumbnail, price }) => {
+      const itemData = {
+        sku: id,
+        name: title,
+        image: thumbnail,
+        salePrice: price,
+      };
+      return itemData;
+    }))
     .catch(() => { throw new Error('Erro ao fazer pesquisa por produtos'); });
-  return fetchMlbItemsById(itemsId);
 }
 
-async function insertDataByDocSelector(selector) {
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function insertDataOnDocSelector(selector) {
   try {
     const importedData = await fetchMlbSearch('computador');
     importedData.forEach((item) => {
       const newItem = createProductItemElement(item);
+      newItem.querySelector('button').addEventListener('click', () => {
+        const li = createCartItemElement(item);
+        const cartItems = document.querySelector('.cart__items');
+        cartItems.appendChild(li);
+      });
       document.querySelector(selector).appendChild(newItem);
     });
   } catch (err) {
@@ -61,21 +89,7 @@ async function insertDataByDocSelector(selector) {
   }
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-window.onload = async function onload() {
-  await insertDataByDocSelector('.items');
+window.onload = function onload() {
+  insertDataOnDocSelector('.items');
+  // fetchMlbSearch('computador');
 };
