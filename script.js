421c2195adb0a0be2f2f155d3e2c3cb1e2fd2d3e
amandaos <amanda.775.souza@gmail.com>
2020-12-29 22:33:54 -0300

    removendo itens da lista de compras

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const parentElement = event.target.parentElement;
+  parentElement.removeChild(event.target);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
