0c44596309a6ca54eaa3c199c3802db24e1d2e72
Giovanna Betti <giovannabetti@gmail.com>
2021-01-02 19:21:45 -0300

    requisite 04

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+function saveItems() {
+  const cart = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('cart', cart);
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+}  
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+}  
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -22,24 +27,25 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+}  
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+}  
 
 function cartItemClickListener(event) {
   const parentElement = event.target.parentElement;
   parentElement.removeChild(event.target);
-}
+  saveItems();
+}  
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
-}
+}  
 
 function generateItemsList() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
@@ -50,13 +56,13 @@ function generateItemsList() {
           sku: item.id,
           name: item.title,
           image: item.thumbnail,
-        };
+        };  
         document
           .querySelector('.items')
           .appendChild(createProductItemElement(obj));
-      }),
-    );
-}
+      }),    
+    );  
+}    
 
 function addItemToCart() {
   document.querySelector('.items').addEventListener('click', (event) => {
@@ -70,16 +76,29 @@ function addItemToCart() {
             sku,
             name: data.title,
             salePrice: data.price,
-          };
+          };  
           document
             .querySelector('.cart__items')
             .appendChild(createCartItemElement(obj));
-        });
+          saveItems();  
+        });  
+    }    
+  });  
+}  
+
+function loadItems() {
+  const loadedCart = localStorage.getItem('cart');
+  const cartList = document.querySelector('.cart__items');
+  cartList.innerHTML = loadedCart;
+  cartList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('cart__item')) {
+      cartItemClickListener(event);
     }
   });
 }
 
 window.onload = function onload() {
+  loadItems();
   generateItemsList();
   addItemToCart();
 };
