1e234d6ac49650feeaebef9ae8b96d8213cdc9b8
Giovanna Betti <giovannabetti@gmail.com>
2021-01-02 19:27:22 -0300

    fixing codeclimate issues

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}  
+}
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}  
+}
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -27,17 +27,17 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}  
+}
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}  
+}
 
 function cartItemClickListener(event) {
   const parentElement = event.target.parentElement;
   parentElement.removeChild(event.target);
   saveItems();
-}  
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -45,7 +45,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   // li.addEventListener('click', cartItemClickListener);
   return li;
-}  
+}
 
 function generateItemsList() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
@@ -56,13 +56,13 @@ function generateItemsList() {
           sku: item.id,
           name: item.title,
           image: item.thumbnail,
-        };  
+        };
         document
           .querySelector('.items')
           .appendChild(createProductItemElement(obj));
-      }),    
-    );  
-}    
+      }),
+    );
+}
 
 function addItemToCart() {
   document.querySelector('.items').addEventListener('click', (event) => {
@@ -76,15 +76,15 @@ function addItemToCart() {
             sku,
             name: data.title,
             salePrice: data.price,
-          };  
+          };
           document
             .querySelector('.cart__items')
             .appendChild(createCartItemElement(obj));
-          saveItems();  
-        });  
-    }    
-  });  
-}  
+          saveItems();
+        });
+    }
+  });
+}
 
 function loadItems() {
   const loadedCart = localStorage.getItem('cart');
