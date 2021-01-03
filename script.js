6c5dfb147e70b62199a534d6006fe1ccae6b4955
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-03 18:49:39 -0300

    Add: requirement 02 - add item to cart.

--- script.js
+++ script.js
@@ -12,22 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -38,6 +22,38 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function addItemToCart(event) {
+  const target = event.target;
+  const targetParentId = target.parentNode.querySelector('.item__sku').innerText;
+  const request = await fetch(`https://api.mercadolibre.com/items/${targetParentId}`);
+  const itemToCart = await request.json();
+
+  const { id:sku, title:name, price:salePrice } = itemToCart;
+  const itemToCartElement = createCartItemElement({ sku, name, salePrice });
+  
+  const cartList = document.querySelector('.cart__items');
+  cartList.appendChild(itemToCartElement);
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+
+  const newButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  newButton.addEventListener('click', addItemToCart);
+  section.appendChild(newButton);
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 async function createProductList(query = 'computador') {
