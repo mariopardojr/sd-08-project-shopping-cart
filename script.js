9223e6a37d1eff98f282b16b858bb6b77e48a4c0
Vinicius <viniciussilvaribeiro@gmail.com>
2021-01-03 15:09:28 -0300

    first commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function totalPrice() {
   listItems.forEach((item) => {
     total += parseFloat(item.innerHTML.split('$')[1]);
   });
-  document.querySelector('.total-price').innerHTML = total;
+  document.querySelector('.total-price').innerHTML = total.toFixed(2);
 }
 
 function createProductImageElement(imageSource) {
