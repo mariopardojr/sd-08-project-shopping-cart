dd09cc7af614d2b88f475517ae27c713489b8998
Bruno Wedley <brunowedley27@gmail.com>
2021-01-03 17:56:24 -0300

    adição dos arquivos até req 4

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -31,13 +29,72 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui - mudança apenas para pull request
+  event.target.parentElement.removeChild(event.target);
+  saveItems();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  //li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function generateItemsList() {
+  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+    .then(response => response.json())
+    .then(data => data.results.forEach(element => {
+      const elementsList = {
+        sku: element.id,
+        name: element.title,
+        image: element.thumbnail,
+      }
+      document.querySelector('.items').appendChild(createProductItemElement(elementsList));
+    }),
+    );
+}
+
+function addItem() {
+  const items = document.querySelector('.items');
+  items.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item__add')) {
+      const parent = event.target.parentElement;
+      const sku = getSkuFromProductItem(parent);
+      fetch(`https://api.mercadolibre.com/items/${sku}`)
+        .then(response => response.json())
+        .then((item) => {
+          const elementsList = {
+            sku,
+            name: item.title,
+            salePrice: item.price,
+          };
+          const cart = document.querySelector('.cart__items');
+          cart.appendChild(createCartItemElement(elementsList));
+          saveItems();
+        });
+    }
+  });
+}
+
+function saveItems() {
+  const cartItem = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('cart', cartItem);
+}
+
+function loadItems() {
+  const savedCart = localStorage.getItem('cart');
+  const cart = document.querySelector('.cart__items')
+  cart.innerHTML = savedCart;
+  cart.addEventListener('click', (event) => {
+    if (event.target.classList.contains('cart__item')) {
+      cartItemClickListener(event);
+    }
+  })
+}
+
+window.onload = function onload() {
+  loadItems();
+  generateItemsList();
+  addItem();
+};
