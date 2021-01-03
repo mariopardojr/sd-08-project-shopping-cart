81f31eb47304d7d72451b6946132d6efca7bea90
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-01-03 19:21:17 -0300

    Correções codeclimate

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-window.onload = function onload() {
-  loadCartLocalStorage();
-  clearCartItems();
-  createItems();
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -34,11 +28,16 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+  updateCartLocalStorage();
+}
+
 const loadCartLocalStorage = () => {
   document.querySelector('.cart__items').innerHTML = localStorage.getItem('cart');
   const cartItems = document.querySelectorAll('.cart__item');
-  cartItems.forEach((e) => e.addEventListener('click', cartItemClickListener));
-}
+  cartItems.forEach(e => e.addEventListener('click', cartItemClickListener));
+};
 
 const clearCartItems = () => {
   const emptyCart = document.querySelector('.empty-cart');
@@ -46,17 +45,12 @@ const clearCartItems = () => {
     document.querySelector('.cart__items').innerHTML = '';
     updateCartLocalStorage();
   });
-}
+};
 
 const updateCartLocalStorage = () => {
   const cartItems = document.querySelector('.cart__items').innerHTML;
   localStorage.setItem('cart', cartItems);
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
-  updateCartLocalStorage();
-}
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -66,31 +60,37 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const addToCart = async (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const id = e.target.parentNode.firstChild.innerText;
-    const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then((e) => e.json());
+const addToCart = async (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const id = event.target.parentNode.firstChild.innerText;
+    const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(e => e.json());
     const { id: sku, title: name, base_price: salePrice } = item;
     const newToCart = createCartItemElement({ sku, name, salePrice });
     newToCart.addEventListener('click', cartItemClickListener);
     document.querySelector('.cart__items').appendChild(newToCart);
     updateCartLocalStorage();
   }
-}
+};
 
 const createItems = async () => {
   const itemsList = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
-    .then((obj) => obj.json())
-    .then((obj) => obj.results);
+    .then(obj => obj.json())
+    .then(obj => obj.results);
 
   const itemsContainer = document.querySelector('.items');
   itemsContainer.innerHTML = '';
 
   itemsList.forEach((e) => {
-    //Olhei o projeto do @paulohbsimoes porque não estava coseguindo passar a url da imagem corretamente
+    // Olhei o projeto do @paulohbsimoes pq não estava coseguindo passar a url da img corretamente
     const { id: sku, title: name, thumbnail: image } = e;
     const newProduct = createProductItemElement({ sku, name, image });
     newProduct.addEventListener('click', addToCart);
     itemsContainer.appendChild(newProduct);
   });
-}
+};
+
+window.onload = function onload() {
+  loadCartLocalStorage();
+  clearCartItems();
+  createItems();
+};
