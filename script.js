9824b8baaafb156970a6eb60eeb0533225b1875f
MuriloBatista <mub4si@gmail.com>
2021-01-05 15:27:18 -0300

    Config gitignore e Req:01

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-window.onload = function onload() {
-
-};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -28,21 +25,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 
 function apiMercado() {
@@ -59,4 +56,6 @@ function apiMercado() {
       });
     });
 }
-apiMercado();
+window.onload = function onload() {
+  apiMercado();
+};
