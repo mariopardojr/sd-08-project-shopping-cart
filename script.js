9142ef8626d4b19ea08aa2872d56e368206a3362
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-01-05 10:50:00 -0400

    adicionando

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
 function getItemPrice(item) {
   return parseFloat(item.innerText.split('PRICE: $')[1]);
 }
@@ -19,13 +23,8 @@ function getTotalPrices() {
   element.innerText = price;
 }
 
-async function addCartItems(event) {
-  const cart = document.querySelector('.cart__items');
-  const item = event.target.parentNode;
-  const sku = getSkuFromProductItem(item);
-  const endPoint = await fetch(`https://api.mercadolibre.com/items/${sku}`);
-  const { id, title, price } = await endPoint.json();
-  cart.appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
+function cartItemClickListener(event) {
+  event.target.parentNode.removeChild(event.target);
   getTotalPrices();
 }
 
@@ -48,21 +47,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  event.target.parentNode.removeChild(event.target);
-  getTotalPrices();
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function addCartItems(event) {
+  const cart = document.querySelector('.cart__items');
+  const item = event.target.parentNode;
+  const sku = getSkuFromProductItem(item);
+  const endPoint = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+  const { id, title, price } = await endPoint.json();
+  cart.appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
+  getTotalPrices();
 }
 
 async function getProducts() {
