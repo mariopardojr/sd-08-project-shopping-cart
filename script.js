005be33dbec92ecdb895817cf46c2a644180d06b
Victor Moraes <moraesvictorgk@gmail.com>
2021-01-06 00:44:40 -0300

    gerar Lista

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,21 +24,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 function gerarLista() {
   const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
@@ -55,9 +54,11 @@ function gerarLista() {
           name: item.title,
           image: item.thumbnail,
         };
-        document.querySelector('.items').appendChild(createProductItemElement(obj));
+        document.getElementsByClassName('items')[0].appendChild(createProductItemElement(obj));
       });
     });
-  }
-  
-  
+}
+
+window.onload = function onload() {
+  gerarLista();
+};
