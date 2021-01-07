1f77b94dacc6e0780762bc977d10f7c8eb8c440e
MuriloBatista <mub4si@gmail.com>
2021-01-07 18:15:19 -0300

    Req:02 codeclimate

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 
-function apiItem(event) {
-  const itemId = event.path[1].firstChild.innerText;
-
-  fetch(`https://api.mercadolibre.com/items/${itemId}`)
-    .then(response => response.json())
-    .then(data => {
-      const newItem = {
-        sku: data.id,
-        name: data.title,
-        salePrice: data.price,
-      }
-      document.querySelector('ol').appendChild(createCartItemElement(newItem));
-    })
-    .catch(error => console.log(error));
-}
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
+}
+
+function cartItemClickListener(event) {
+
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function apiItem(event) {
+  const itemId = event.path[1].firstChild.innerText;
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    .then(response => response.json())
+    .then((data) => {
+      const cartItem = {
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      };
+      document.querySelector('ol').appendChild(createCartItemElement(cartItem));
+    });
 }
 
 function createCustomElement(element, className, innerText) {
@@ -29,8 +39,6 @@ function createCustomElement(element, className, innerText) {
   if (element === 'button') {
     e.addEventListener('click', apiItem);
   }
-
-
   return e;
 }
 
@@ -49,19 +57,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-
-}
-
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function apiMercado() {
