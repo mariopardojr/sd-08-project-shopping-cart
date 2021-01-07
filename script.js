681a44359d055f56ace2069a0f8a9217af7fe144
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-01-07 17:44:12 -0300

    req1: listagem de produtos na pagina por meio de requisicao fetch

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,30 +12,50 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+async function productsByQuery(word, loadFn) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${word}`;
+  try {
+    const response = await fetch(url);
+    const { results } = await response.json();
+    loadFn(results);
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const loadProducts = (products) => {
+  const itemSection = document.querySelector('.items');
+  products.forEach((cartItem) => {
+    itemSection.appendChild(createProductItemElement(cartItem));
+  });
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  productsByQuery('computer', loadProducts);
+};
