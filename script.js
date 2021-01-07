fe6cdd6f6714a779921f6996e0540419f92e0c50
Antônio Schappo <antonioschappo@gmail.com>
2021-01-07 16:09:20 -0300

    Codeclimate fix5 req. 2

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function createProductImageElement(imageSource) {
 }
 
 function cartItemClickListener(event) {
-  event.target
+event.target.addEventListener('click', () => {
+event.target.remove();
+});
 }
 
 function getSkuFromProductItem(item) {
