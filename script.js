185f93cadc2361cbd43040ac6991dcca9936a448
Uilton Rocha <itoclick@gmail.com>
2021-01-08 17:02:36 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -60,13 +60,11 @@ const tocart = async (event) => {
   fetch(`https://api.mercadolibre.com/items/${ids}`)
     .then(objapi => objapi.json())
     .then((objson) => {
-      
       ({
         sku: objson.id,
         name: objson.title,
         salePrice: objson.price,
       })
-     
     })
     .then(objparam => createCartItemElement(objparam));
 };
