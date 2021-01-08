44abb5f19deedb5d868500f04ae1a86ee8ec49ac
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-08 05:33:39 -0300

    code climate

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(
-    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
   );
 
   return section;
@@ -58,7 +57,7 @@ function generateList() {
           .appendChild(createProductItemElement(obj));
       })
     );
-}
+};
 
 function addToCart() {
   document.querySelector('.items').addEventListener('click', (event) => {
