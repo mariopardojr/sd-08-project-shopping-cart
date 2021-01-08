d2c9e9954729fbaac4b006ceba3ffef6a3702a7b
Doug-Fernando <doug.fernando95@gmail.com>
2021-01-08 13:42:50 -0300

    corrige codeclimate

--- script.js
+++ script.js
@@ -47,11 +47,11 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 const fetchItems = async () => {
   const req = await fetch(API_URL);
   const { results } = await req.json();
-    document.querySelector('.loading').remove();
-    results.forEach((item) => {
-      const section = document.querySelector('.items');
-      section.append(createProductItemElement(item));
-    });
+  document.querySelector('.loading').remove();
+  results.forEach((item) => {
+    const section = document.querySelector('.items');
+    section.append(createProductItemElement(item));
+  });
 };
 
 
