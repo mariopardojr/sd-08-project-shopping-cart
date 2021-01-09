380daa8d8761eeb93c6602a1b48f5f40414c1f76
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-01-09 17:20:45 -0300

    Primeira tarefa

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -41,3 +39,22 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+// promise como objeto{}
+// function fetchAndRenderProducts() {
+//   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+//     .then(response => response.json())
+//     .then(data => data);
+// }
+
+async function fetchAndRenderProducts() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const { results } = await response.json();
+  results.forEach(product => {
+    const productItem = createProductItemElement(product);
+    document.querySelector('.items').appendChild(productItem);
+  });
+}
+
+window.onload = function onload() {
+  fetchAndRenderProducts();
+};
