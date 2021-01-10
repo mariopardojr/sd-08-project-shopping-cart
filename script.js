eda7dd99fdc40496165068dd66ae25b76dc77890
Pedro Alves <ped.alm2709@gmail.com>
2021-01-09 22:42:13 -0300

    requisito 3

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -41,20 +49,15 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
-  // coloque seu código aqui
+function cartItemClickListener(event) {
+  const removeItem = document.getElementsByTagName('ol')[0];
+  removeItem.addEventListener('click', () => {
+    document.querySelector('.cart__items').removeChild(event.target);
+  });
 }
 
 function getApi() {
