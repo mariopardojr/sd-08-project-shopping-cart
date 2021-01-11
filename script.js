2222218c324888e54bdcc1ffd89016ef0c047837
Antônio Schappo <antonioschappo@gmail.com>
2021-01-10 22:30:48 -0300

    codeclimate(4) fix req. 6

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function cartItemClickListener(event) {
   event.target.remove();
   const cartSection = document.getElementsByClassName('cart__items')[0];
   const splitTotal = [...cartSection.childNodes].reduce((acc, curr) =>
-    acc += parseFloat(curr.innerText.split('$')[1]), 0);
+    acc + parseFloat(curr.innerText.split('$')[1]), 0);
   const array = [...cartSection.childNodes].map(item => item.innerText);
   localStorage.setItem('cart', JSON.stringify(array));
   localStorage.setItem('total', splitTotal);
