fc176e188d98fd1832cf78196c1d62d2f8b1884c
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-01-13 17:29:53 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,18 +24,46 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  
-}
+const filterResultObject = (array) => {
+  const idNameImageProducts = array
+  .map(({ id, title, thumbnail}) => ({ sku: id, name: title, image: thumbnail}))
+  .forEach((element) => {
+    const addItens = document.querySelector('.items');
+    addItens.appendChild(createProductItemElement(element));
+  });
+  return idNameImageProducts;
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+const fetchProducts = product => {
+  const productByCategory = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+  fetch(productByCategory)
+    .then(response => response.json())
+    .then(object => {
+      if(object.error) {
+        throw new Error(object.error);
+      } else {
+        filterResultObject(object.results);
+      }
+    });
+};
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+window.onload = function onload() {
+  fetchProducts('computador')
+};
