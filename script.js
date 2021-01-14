4b1041547171ba9c21640e5416354aeacf3e8920
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-01-14 14:33:51 -0300

    There is always time to Ontrackear

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
-
+// Verificando Fluxo Git
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
