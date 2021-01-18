a843a5bbad211d96aa26158f1b3b0d34f68a333f
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-01-18 15:41:07 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -31,7 +29,6 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +38,20 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+async function fetchAndRenderizeProducts() {
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const { results } = await response.json();
+    results.forEach((product) => {
+      const productItem = createProductItemElement(product);
+      document.querySelector('.items').appendChild(productItem);
+    });
+  } catch (error) {
+    console.log('erro aqui', error);
+  }
+}
+
+window.onload = function onload() {
+  fetchAndRenderizeProducts();
+};
