2d1529d0390cdd12c4de2de2582e37d0c12452be
Andre Sampaio <andresampaiolm@gmail.com>
2021-01-20 18:33:09 -0300

    price +1

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
 
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice + 1}`;
   li.addEventListener('click', cartItemClickListener);
 
   ol.appendChild(li);
