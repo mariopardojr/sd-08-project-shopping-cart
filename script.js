05794fa1cb5a39f0278f8e2048ba38ff2a4f11c7
Víctor Cançado <victor.cancado@gmail.com>
2021-01-20 23:44:40 -0300

    Adiciona req 6

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function totalPrice() {
+  let total = 0;
+  document.querySelectorAll('.cart__item').forEach(item => {
+    total += parseFloat(item.innerText.split('$')[1]);
+  });
+  document.querySelector('.total-price').innerHTML = total;
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,38 +20,28 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  button.addEventListener('click', () => {
-    //fetch()
-  });
-  section.appendChild(button);
-  
-  return section;
+function addLocalStorage() {
+  const completedList = document.querySelector('.cart__items');
+  localStorage.setItem('eachItem', completedList.innerHTML);
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function getLocalStorage() {
+  const completedList = document.querySelector('.cart__items');
+  completedList.innerHTML = localStorage.getItem('eachItem');
+  totalPrice();
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
+  totalPrice();
+  addLocalStorage();
 }
 
-// function getId() {
-//   const liId = document.querySelector('.item__sku');
-//   const buttonClass = document.querySelector('.items');
-//   buttonClass.addEventListener('click', () => console.log(buttonClass));
-  
-// }
-
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({
+  id: sku,
+  title: name,
+  base_price: salePrice,
+}) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -51,16 +49,52 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement(
+    'button',
+    'item__add',
+    'Adicionar ao carrinho!',
+  );
+  button.addEventListener('click', async () => {
+    const getNewApi = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+    const result = await getNewApi.json();
+    const cartItem = createCartItemElement(result);
+    document.querySelector('.cart__items').appendChild(cartItem);
+    totalPrice();
+    addLocalStorage();
+  });
+  section.appendChild(button);
+  return section;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
 async function fetchAPI() {
-  const apiResponse = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const apiResponse = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/search?q=computador',
+  );
   const { results } = await apiResponse.json();
-  results.forEach((computer) => {
+  results.forEach(computer => {
     const newComputer = createProductItemElement(computer);
     document.querySelector('.items').appendChild(newComputer);
   });
 }
 
+document.querySelector('.empty-cart').addEventListener('click', () => {
+  document.querySelector('.cart__items').innerHTML = '';
+  totalPrice();
+  addLocalStorage();
+});
+
 window.onload = function onload() {
   fetchAPI();
-  getId();
+  getLocalStorage();
 };
