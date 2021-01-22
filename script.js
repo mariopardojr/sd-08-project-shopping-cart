f8eff547b5f7249aa8c30a1f5fdfdb0dd0e7c0ec
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-01-22 01:14:47 -0300

    alteraçoes e ajustes

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener() {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function buttonOnClick() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(response => response.json())
+    .then((data) => {
+      const cartItems = document.querySelector('.cart__items');
+      cartItems.appendChild(createCartItemElement(data));
+    });
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -30,27 +51,6 @@ function createProductItemElement({ sku, name, image }) {
 //  return item.querySelector('span.item__sku').innerText;
 // }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function buttonOnClick() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then((data) => {
-      const cartItems = document.querySelector('.cart__items');
-      cartItems.appendChild(createCartItemElement(data));
-    });
-}
-
 function fetchAndRenderProducts() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then(response => response.json())
@@ -62,6 +62,6 @@ function fetchAndRenderProducts() {
   });
 }
 
-window.onload = function onload() { 
+window.onload = function onload() {
   fetchAndRenderProducts();
 };
