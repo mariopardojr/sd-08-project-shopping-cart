8d2e60ecc19d557111b10d76ffd19b8d0e4c607e
Thais Quintela <thais.s.quintela@gmail.com>
2021-01-22 01:50:42 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -28,21 +28,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
-  const cartItems = document.querySelector('.cart__items');
-  cartItems.addEventListener('click', (event) => {
-    event.target.remove();
-  });
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function fetchData() {
   const loading = document.querySelector('.loading');
   loading.innerText = 'loading...';
@@ -64,12 +49,42 @@ function fetchData() {
     });
 }
 
+let totalCartPrice = 0;
+const showPrice = document.createElement('h3');
+
+async function totalPrice(price) {
+  const actualPrice = document.querySelector('.total-price').appendChild(showPrice);
+  const cartItems = document.querySelector('.cart__items').getElementsByClassName('cart__item');
+  if (cartItems.length !== 0) {
+    totalCartPrice += price;
+  }
+  actualPrice.innerHTML = `${totalCartPrice}`;
+}
+
+function cartItemClickListener() {
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.addEventListener('click', (event) => {
+    event.target.remove();
+  });
+  const findText = event.target.innerText.split('$');
+  const findPrice = -(parseInt(findText[1], 10));
+  totalPrice(findPrice);
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function addToCart() {
   const items = document.querySelector('.items');
-  items.addEventListener('click', (event) => {
+  items.addEventListener('click', async (event) => {
     if (event.target.className === 'item__add') {
       const itemSku = event.target.parentElement.querySelector('.item__sku').innerText;
-      fetch(`https://api.mercadolibre.com/items/${itemSku}`)
+      await fetch(`https://api.mercadolibre.com/items/${itemSku}`)
         .then((response) => {
           response.json()
             .then(((productData) => {
@@ -79,18 +94,20 @@ function addToCart() {
                 salePrice: productData.price,
               };
               document.querySelector('.cart__items').appendChild(createCartItemElement(itemData));
+              totalPrice(itemData.salePrice);
             }));
         });
     }
   });
 }
-// Dei uma olhada no requisito 2 do vídeo de passo a passo que fizeram https://trybecourse.slack.com/archives/C01A9A2N93R/p1608237982190300 , porque não estava conseguindo acertar o addEventListener, peguei a dica de adicionar um só para todo o container e depois isolar os botões.
+// Dei uma olhada no requisito 2 do vídeo de passo a passo que fizeram https://trybecourse.slack.com/archives/C01A9A2N93R/p1608237982190300 , porque não estava conseguindo acertar o addEventListener quando tentava adicionar um para cada item, peguei a dica de adicionar um só para todo o container e depois isolar os botões.
 
 function emptyCart() {
   const cart = document.querySelector('.empty-cart');
   cart.addEventListener('click', () => {
     document.querySelectorAll('.cart__item').forEach((element) => {
       element.remove();
+      document.querySelector('.total-price').appendChild(showPrice).innerText = 0;
     });
   });
 }
